b8cf937e24107ef54b6ee13a1fa1f92d82142958
Saishreehas <89503791+Saishreehas@users.noreply.github.com>
2022-11-06 17:51:29 -0800

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,32 @@
+var plane,planeimg
+var backgroundimg
+var health,healthimg
+var gas,gasimg
+var nukes,nukesimg
+function preload() {
+backgroundimg=loadImage("Cloud.jpg")
+planeimg=loadImage("Plane.png")
+healthimg=loadImage("health.png")
+//gasimg=loadImage("Gas.png")
+//nukesimg=loadImage("nukesfromrussia.png")
+}
+
+
+function setup() {
+  createCanvas(1500,600)
+    plane=createSprite(700,500,10,10)
+    plane.addImage("plane",planeimg)
+    plane.scale=1
+    health=createSprite(1000,100,100)
+    health.addImage("health",healthimg)
+    health.scale=0.1
+
+
+
+}
+
+function draw() {
+  background(backgroundimg);  
+  drawSprites();
+}
+
